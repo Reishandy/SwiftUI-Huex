39457a3cf556ddb407b29fa62ae371d9cc4404ba
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-07-16 11:40:09 +0800

    Initial Commit

---
+++ Huex/HuexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuexApp.swift
+//  Huex
+//
+//  Created by Muhammad Akbar Reishandy on 16/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HuexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
